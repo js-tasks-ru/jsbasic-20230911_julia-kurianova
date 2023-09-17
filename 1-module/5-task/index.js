44a2ee605d007661777c38d362a9cc1b457d7530
liakurianova <julia.kurianova@gmail.com>
2023-09-17 14:35:30 -0400

    truncated string

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,9 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  const length = str.length;
+  const charToEnd = String.fromCharCode(0x2026); // ellipsis
+
+  if (length <= maxlength) return str;
+  const truncWord = str.slice(0, maxlength - 1);
+
+  return truncWord + charToEnd;
 }
