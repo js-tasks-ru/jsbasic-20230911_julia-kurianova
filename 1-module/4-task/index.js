756e27cff624a2176e57410eb38830d5e2b81b7f
liakurianova <julia.kurianova@gmail.com>
2023-09-17 14:05:38 -0400

    check spam finction

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
-  // ваш код...
+  const spamWord1 = "1xBet".toLowerCase();
+  const spamWord2 = "XXX".toLowerCase();
+  str = str.toLowerCase();
+
+  if (str.includes(spamWord1) || str.includes(spamWord2)) return true;
+
+  return false;
 }
