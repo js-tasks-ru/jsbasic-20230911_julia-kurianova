7a71a1b2304bc202c784264bb4ad80663e3cad86
liakurianova <julia.kurianova@gmail.com>
2023-09-17 12:34:11 -0400

    turn first character into upper case

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,7 @@
 function ucFirst(str) {
-  // ваш код...
+  if (Object.is(str, null) || Object.is(str, undefined))
+    return "no string in argument";
+  if (str.length < 2) return str.toUpperCase();
+
+  return str[0].toUpperCase() + str.slice(1);
 }
