276db8a53f6905798f8f435e90d99284000a08b7
liakurianova <julia.kurianova@gmail.com>
2023-09-18 12:50:56 -0400

    remove unnecessary check

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -2,8 +2,5 @@ function checkSpam(str) {
   const spamWord1 = "1xBet".toLowerCase();
   const spamWord2 = "XXX".toLowerCase();
   str = str.toLowerCase();
-
-  if (str.includes(spamWord1) || str.includes(spamWord2)) return true;
-
-  return false;
+  return str.includes(spamWord1) || str.includes(spamWord2);
 }
