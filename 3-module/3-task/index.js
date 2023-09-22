eec3c045fd1478cc1f528719359303c1d41c6ac8
liakurianova <julia.kurianova@gmail.com>
2023-09-21 21:41:38 -0400

    Task-3. Camelize function

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,16 @@
 function camelize(str) {
-  // ваш код...
+  if (!str) return str;
+
+  const startWithHyphen = str[0] === "-";
+  const result = str
+    .split("-")
+    .filter((item) => item)
+    .map((item) => item[0].toUpperCase() + item.slice(1, item.length))
+    .join("");
+
+  if (startWithHyphen) {
+    return result;
+  } else {
+    return result[0].toLowerCase() + result.slice(1, result.length);
+  }
 }
