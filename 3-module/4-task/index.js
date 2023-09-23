9e0499a3cb7d37658a62b5e9f39da76cfb4c7510
liakurianova <julia.kurianova@gmail.com>
2023-09-23 17:04:08 -0400

    Fix showSalary function

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,7 +1,6 @@
 function showSalary(users, age) {
   return users
     .filter((item) => item.age <= age)
-    .map((item) => `${item.name}, ${item.balance}\n`)
-    .join("")
-    .slice(0, -1);
+    .map((item) => `${item.name}, ${item.balance}`)
+    .join("\n");
 }
