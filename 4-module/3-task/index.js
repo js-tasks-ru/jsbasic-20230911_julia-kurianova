5d3ca1c3dcb144d4d65af3a884942519f7e0847f
liakurianova <julia.kurianova@gmail.com>
2023-09-26 22:47:00 -0400

    highlight function

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,33 @@
 function highlight(table) {
-  // ваш код...
+  const rows = Array.from(table.rows);
+  rows.shift();
+
+  rows.forEach((row) => {
+    const cells = Array.from(row.cells);
+
+    cells.forEach((cell) => {
+      // Status column
+      if (cell.cellIndex === 3) {
+        if (!Object.hasOwn(cell.dataset, "available")) {
+          row.setAttribute("hidden", true);
+        }
+        if (cell.dataset.available === "true") {
+          row.classList.add("available");
+        }
+        if (cell.dataset.available === "false") {
+          row.classList.add("unavailable");
+        }
+      }
+      // Gender column
+      if (cell.cellIndex === 2) {
+        if (cell.innerText === "m") row.classList.add("male");
+        if (cell.innerText === "f") row.classList.add("female");
+      }
+      // Age column
+      if (cell.cellIndex === 1) {
+        if (Number(cell.innerText) < 18)
+          row.style.textDecoration = "line-through";
+      }
+    });
+  });
 }
